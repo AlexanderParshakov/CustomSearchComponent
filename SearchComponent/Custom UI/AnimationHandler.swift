0bfdd954d44874f289c8d72e6aac77bd324d69c7
AlexanderParshakov <alls.parshakov@gmail.com>
2019-10-30 14:00:34 +0300

    Classes to structs.

--- SearchComponent/Custom UI/AnimationHandler.swift
+++ SearchComponent/Custom UI/AnimationHandler.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Lottie
 
-final class AnimationHandler {
+struct AnimationHandler {
     
     private init() {}
     
--- SearchComponent/Networking/NetworkManager.swift
+++ SearchComponent/Networking/NetworkManager.swift
@@ -10,7 +10,7 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-final class NetworkManager {
+struct NetworkManager {
     
     private init() {}
     
